976116d787ce89953366a3e9083c8da0752ef930
Rehan Jaffer <ray@thelondonvandal.com>
2016-05-18 11:53:10 +0100

    committed db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160517114614) do
+ActiveRecord::Schema.define(version: 20160517140708) do
 
   create_table "pages", force: :cascade do |t|
     t.string   "virtual_path", null: false
     t.text     "content",      null: false
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "title"
   end
 
 end
